15feec6d1af103875078b18a8a0d4365a301d07d
Ron Raithatha <ronaksraithatha@gmail.com>
2016-02-13 17:02:10 -0500

    solved some

--- app/arrays.js
+++ app/arrays.js
@@ -3,39 +3,51 @@ exports = (typeof window === 'undefined') ? global : window;
 exports.arraysAnswers = {
 
   indexOf : function(arr, item) {
-
+    return arr.indexOf(item);
   },
 
   sum : function(arr) {
-
+    return arr.reduce(function(a,b) {return a + b;});
   },
 
   remove : function(arr, item) {
-
+    return arr.filter(function(a) { return a != item; } );
   },
 
   removeWithoutCopy : function(arr, item) {
-
+    for(var i = 0, len = arr.length; i < len; i++){
+      if (arr[i] == item) {
+        arr.splice(i, 1);
+        i --;
+        len --;
+      }
+    }
+    return arr;
   },
 
   append : function(arr, item) {
-
+    arr.push(item);
+    return arr;
   },
 
   truncate : function(arr) {
-
+    arr.pop()
+    return arr;
   },
 
   prepend : function(arr, item) {
-
+    arr.unshift(item)
+    return arr;
   },
 
   curtail : function(arr) {
-
+    arr.shift()
+    return arr;
   },
 
   concat : function(arr1, arr2) {
-
+    arr2.map(function(a){arr1.push(a);});
+    return arr1;
   },
 
   insert : function(arr, item, index) {
